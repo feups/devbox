4df1944ace4b9bfc64087714c52c96a1d6a5b0a4
Pierre Radermecker <pradermecker@cirb.brussels>
2016-11-20 17:48:15 +0100

    Revert "Add hiera doc"
    This reverts commit 52fac7393044b6ee5d3ec40c4d22234d0a1eef7a.

--- user/setenv.sh
+++ user/setenv.sh
@@ -48,7 +48,6 @@ function install_commonfiles {
 
     echo "Installing doc files"
     curl -s http://stash.cirb.lan/projects/CICD/repos/puppet-shared-scripts/raw/README.adoc?at=refs/heads/master > puppet.adoc
-    curl -s http://stash.cirb.lan/projects/CICD/repos/hieradata/raw/README.adoc?at=refs/heads/master > hiera.adoc
     make doc
     install -Dm644 doc/devbox.html "${HOME}/.local/share/doc/devbox.html"
     install -Dm644 doc/devbox.pdf "${HOME}/.local/share/doc/devbox.pdf"
