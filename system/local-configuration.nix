a82b667ef3bcd25537e952e4407ebbddd6f67ef7
Pierre Radermecker <pradermecker@cirb.brussels>
2016-12-02 09:58:04 +0100

    Switch from bash to zsh shell

--- system/local-configuration.nix
+++ system/local-configuration.nix
@@ -8,6 +8,27 @@
        plugins = [ ];
      })
     haskellPackages.language-puppet
+    oh-my-zsh
+    zsh
     # geany
   ];
+
+  programs.zsh.enable = true;
+  programs.zsh.interactiveShellInit = ''
+    export ZSH=${pkgs.oh-my-zsh}/share/oh-my-zsh/
+  '';
+  programs.zsh.shellAliases = {
+    la = " ls -alh";
+    ls = " ls --color=tty";
+    duh = " du -h --max-depth=1";
+    df = " df -h";
+    ag = "ag --color-line-number=3";
+    vim = "nvim";
+    build = "./build/build.sh";
+    see = "./bin/check_role.sh";
+    heyaml= "./bin/eyaml.sh $@";
+    fixlint = "./bin/fix-lint.sh";
+    nixreb = "sudo nixos-rebuild switch";
+  };
+  users.defaultUserShell = "/run/current-system/sw/bin/zsh";
 }
