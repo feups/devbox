09e4edee4a247d280ed6965c4c2027963ddc3f58
Pierre Radermecker <pradermecker@cirb.brussels>
2017-01-17 12:10:44 +0100

    Move zsh config from local into configuration.nix

--- system/configuration.nix
+++ system/configuration.nix
@@ -101,6 +101,7 @@
     nfs-utils
     nodejs
     rsync
+    oh-my-zsh
     parallel
     python
     shellcheck
@@ -116,6 +117,8 @@
     xsel
     zeal
     zip
+    zsh
+    zsh-completions
   ];
 
   # Creates a "vagrant" users with password-less sudo access
@@ -174,6 +177,28 @@
 
     #. $(autojump-share)/autojump.bash
   '';
+
+  programs.zsh.enable = true;
+  programs.zsh.interactiveShellInit = ''
+    export ZSH=${pkgs.oh-my-zsh}/share/oh-my-zsh/
+  '';
+  programs.zsh.shellAliases = {
+    la = " ls -alh";
+    ls = " ls --color=tty";
+    ll = "ls -lh";
+    duh = " du -h --max-depth=1";
+    df = " df -h";
+    ag = "ag --color-line-number=3";
+    vim = "nvim";
+    build = "./build/build.sh";
+    see = "./bin/check_role.sh";
+    heyaml = "./bin/eyaml.sh $@";
+    fixlint = "./bin/fix-lint.sh";
+    nixreb = "sudo nixos-rebuild switch";
+    ldir = "ls -ladh (.*|*)(/,@)";
+    lfile = "ls -lah *(.)";
+  };
+  users.defaultUserShell = "/run/current-system/sw/bin/zsh";
 
   system.stateVersion = "16.09";
 
--- system/local-configuration.nix
+++ system/local-configuration.nix
@@ -8,31 +8,7 @@
        plugins = [ ];
      })
     firefox
-    oh-my-zsh
-    zsh-completions
-    zsh
     # geany
   ];
 
-  programs.zsh.enable = true;
-  programs.zsh.interactiveShellInit = ''
-    export ZSH=${pkgs.oh-my-zsh}/share/oh-my-zsh/
-  '';
-  programs.zsh.shellAliases = {
-    la = " ls -alh";
-    ls = " ls --color=tty";
-    ll = "ls -lh";
-    duh = " du -h --max-depth=1";
-    df = " df -h";
-    ag = "ag --color-line-number=3";
-    vim = "nvim";
-    build = "./build/build.sh";
-    see = "./bin/check_role.sh";
-    heyaml = "./bin/eyaml.sh $@";
-    fixlint = "./bin/fix-lint.sh";
-    nixreb = "sudo nixos-rebuild switch";
-    ldir = "ls -ladh (.*|*)(/,@)";
-    lfile = "ls -lah *(.)";
-  };
-  users.defaultUserShell = "/run/current-system/sw/bin/zsh";
 }
