a93dc089d59e4315de2d9e7827cd8934f00aded8
Pierre Radermecker <pradermecker@cirb.brussels>
2017-02-01 08:58:37 +0100

    Add comments in params.sh

--- user/params.sh
+++ user/params.sh
@@ -1,8 +1,14 @@
-user_name=
-user_email=
-user_id=
-user_pwd=
-user_stack=
+# Your full user name e.g "John Doe"
+user_name=""
+# The email address you want to be registered for your 'git commit'
+user_email=""
+# Your Active directory id (e.g "jdoe")
+user_id=""
+# Your Active directory password used by the `cicd` command line
+user_pwd=""
+# The default stack name you are working with. This will enabled the `mr`  puppet-stack-$stackname repo
+# eg. "bos"
+user_stack=""
 eclipse_plugins=true
 eclipse_geppetto=true
 mr_template_repo_url="git://github.com/CIRB/vcsh_mr_template.git"
