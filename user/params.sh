278f130c59b634317aca0f919da3269b3545d706
Pierre Radermecker <pradermecker@cirb.brussels>
2017-02-01 09:41:28 +0100

    Minor documentation changes

--- user/params.sh
+++ user/params.sh
@@ -8,11 +8,14 @@ user_name=""
 user_email=""
 # Your Active directory id (e.g "jdoe")
 user_id=""
-# Your Active directory password used by the `cicd` command line
+# Your Active directory password. It is used by the `cicd` command line (orchestration).
 user_pwd=""
 # The default stack name you are working with. This will enabled the `mr`  puppet-stack-$stackname repo
 # eg. "bos"
 user_stack=""
+# Do you want to download eclipse plugins such as egit or m2e ?
 eclipse_plugins=true
+# Do you want to download the eclipse plugin geppetto (puppet supported plugin)
 eclipse_geppetto=true
+# Enter the url of your fork of the `vcsh_mr_template` if you maintain one.
 mr_template_repo_url="git://github.com/CIRB/vcsh_mr_template.git"
