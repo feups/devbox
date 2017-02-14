7357adc4472630070dc0b65d3411a03ce08c9829
Pierre Radermecker <pradermecker@cirb.brussels>
2017-02-14 11:52:21 +0100

    Update language-puppet to 1.3.5 for the cicd shell

--- user/pkgs/cicd-shell/share/default.nix
+++ user/pkgs/cicd-shell/share/default.nix
@@ -9,7 +9,7 @@ with import (bootstrap.fetchFromGitHub {
   }) { };
 stdenv.mkDerivation {
   name = "pepper-env";
-  buildInputs = [ pepper jq haskellPackages.language-puppet_1_3_4 ];
+  buildInputs = [ pepper jq haskellPackages.language-puppet ];
   shellHook = ''
   export SALTAPI_USER="${salt-user}"
   export SALTAPI_PASS="${salt-pass}"
@@ -17,7 +17,16 @@ stdenv.mkDerivation {
   export SALTAPI_EAUTH=ldap
   export ZONE="${zone}"
   export PS1="\n\[\033[1;32m\][cicd ${zone}]$\[\033[0m\] "
-  alias cicd="cicd $ZONE"
+  unalias -a
+  alias facts="cicd ${zone} facts"
+  alias du="cicd ${zone} du"
+  alias data="cicd ${zone} data"
+  alias stats="cicd ${zone} stats"
+  alias ping="cicd ${zone} ping"
+  alias runpuppet="cicd ${zone} runpuppet"
+  alias sync="cicd ${zone} sync"
+  alias result="cicd ${zone} result"
+  alias gentags="cicd ${zone} gentags"
   alias pep="pepper"
   '';
 }
